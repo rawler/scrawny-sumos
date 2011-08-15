8dec0dcc5af72a8670f96bb1e2d7e3293e480dff
Ulrik Mikaelsson <ulrik.mikaelsson@gmail.com>
2011-08-15 13:00:15 +0200

    Rewrite body-structure in Player.

--- sumo.py
+++ sumo.py
@@ -5,20 +5,74 @@ from pygame.locals import *
 from pygame.color import *
 import pymunk as pm
 import math, sys, random
-X,Y,Z = 0,1,2 # Easy indexing
+
+LEG_LEN = 35
+MUSCLE_STRENGTH = 4000000
+MUSCLE_STIFFNESS = 20000
 
 class Player(object):
-    def __init__(self, thigh_muscle, calf_muscle, direction):
-        self.thigh_muscle = thigh_muscle
-        self.calf_muscle = calf_muscle
+    def __init__(self, world, shared_body, direction):
+        self.world = world
+        self.shared_body = shared_body
         self.direction = direction
         self.stretch = (math.pi/4)
         self.angle = 0
         self.score = 0
 
+        self._setup()
+
+    def _setup(self):
+        direction = self.direction
+        self.head = pm.Circle(self.shared_body, 25, (50*direction, 55))
+
+        self.thigh = pm.Body(10, 10000)
+        self.calf = pm.Body(10, 10000)
+
+        self.segments = [
+            pm.Segment(self.thigh, (0,0), (0,-LEG_LEN), 5.0),
+            pm.Segment(self.calf, (0,0), (0,-LEG_LEN), 5.0),
+        ]
+
+        self.reset()
+
+        hip_joint = pm.PinJoint(self.shared_body, self.thigh, (50*direction,0), (0,0))
+        knee_joint = pm.PinJoint(self.thigh, self.calf, (0,-LEG_LEN), (0,0))
+
+        self.thigh_muscle = pm.DampedRotarySpring(self.shared_body, self.thigh, 0, MUSCLE_STRENGTH, MUSCLE_STIFFNESS)
+        self.calf_muscle = pm.DampedRotarySpring(self.thigh, self.calf, 0, MUSCLE_STRENGTH, MUSCLE_STIFFNESS)
+
+        self.foot = pm.Circle(self.calf, 1, (0,-LEG_LEN))
+        self.foot.friction = 0.6
+        self.world.add(self.head, self.thigh, self.calf, self.segments, hip_joint, knee_joint, self.thigh_muscle, self.calf_muscle, self.foot)
+        self.updateMuscles()
+
     def updateMuscles(self):
         self.thigh_muscle.rest_angle = (self.angle + self.stretch) * self.direction
         self.calf_muscle.rest_angle = (-self.stretch*2) * self.direction
+
+    def reset(self):
+        self.thigh.position = 300+(50*self.direction),300
+        self.calf.position = 300+(50*self.direction),300-LEG_LEN
+        for obj in (self.thigh, self.calf):
+            obj.angle = 0
+            obj.velocity = (0,0)
+            obj.angular_velocity = 0
+            obj.reset_forces()
+
+    def draw(self, screen):
+        head = self.head
+        body = head.body
+        cv = body.position + head.offset.rotated(body.angle)
+        c = to_pygame(cv)
+        pygame.draw.circle(screen, THECOLORS["black"], c, int(head.radius))
+
+        for line in self.segments:
+            body = line.body
+            pv1 = body.position + line.a.rotated(body.angle)
+            pv2 = body.position + line.b.rotated(body.angle)
+            pt1 = to_pygame(pv1)
+            pt2 = to_pygame(pv2)
+            pygame.draw.lines(screen, THECOLORS["black"], False, [pt1,pt2], 4)
 
 def to_pygame(p):
     """Small hack to convert pymunk to pygame coordinates"""
@@ -52,63 +106,33 @@ def main():
     space.add(ground, mat)
 
     ### The moving L shape
-    l_head = (25, (-50, 55))
     l_shoulder = (-50, 30)
     l_hip = (-50,0)
-
-    r_head = (25, (50, 55))
     r_shoulder = (50, 30)
     r_hip = (50,0)
-    r_knee = (30,-20)
-    r_foot = (50,-40)
 
     body = pm.Body(10,10000)
-
-    LEG_LEN = 35
-
-    left_thigh = pm.Body(10, 10000)
-    left_calf = pm.Body(10, 10000)
-
-    right_thigh = pm.Body(10, 10000)
-    right_calf = pm.Body(10, 10000)
 
     lines = [pm.Segment(body, l_shoulder, l_hip, 5.0) 
             ,pm.Segment(body, r_shoulder, r_hip, 5.0)
             ,pm.Segment(body, r_shoulder, l_hip, 5.0)
             ,pm.Segment(body, l_shoulder, r_hip, 5.0)
-
-            ,pm.Segment(left_thigh, (0,0), (0,-LEG_LEN), 5.0)
-            ,pm.Segment(left_calf, (0,0), (0,-LEG_LEN), 5.0)
-            ,pm.Segment(right_thigh, (0,0), (0,-LEG_LEN), 5.0)
-            ,pm.Segment(right_calf, (0,0), (0,-LEG_LEN), 5.0)
     ]
 
-    left_head = pm.Circle(body, *l_head)
-    right_head = pm.Circle(body, *r_head)
+    space.add(body, lines)
+    body.position = 300,300
 
-    left_foot = pm.Circle(left_calf, 1, (0,-LEG_LEN))
-    right_foot = pm.Circle(right_calf, 1, (0,-LEG_LEN))
-    left_foot.friction = 0.6
-    right_foot.friction = 0.6
-
-    feet = [left_foot, right_foot]
-
-    space.add(body, lines, left_head, right_head, feet, left_thigh, left_calf, right_thigh, right_calf)
-
-    MUSCLE_STRENGTH = 4000000
-    MUSCLE_STIFFNESS = 10000
+    P1 = Player(space, body, -1.0)
+    P2 = Player(space, body, 1.0)
 
     def reset():
         body.position = (300,300)
-        left_thigh.position = 250,300
-        left_calf.position = 250,300-LEG_LEN
-        right_thigh.position = 350,300
-        right_calf.position = 350,300-LEG_LEN
-        for obj in (body, left_thigh, left_calf, right_thigh, right_calf):
-            obj.angle = 0
-            obj.velocity = (0,0)
-            obj.angular_velocity = 0
-            obj.reset_forces()
+        body.angle = 0
+        body.velocity = (0,0)
+        body.angular_velocity = 0
+        body.reset_forces()
+        P1.reset()
+        P2.reset()
 
     def kill(player):
         if player is P1:
@@ -120,35 +144,20 @@ def main():
 
     def onCollision(space, arbiter):
         shapes = arbiter.shapes
-        if left_head in shapes:
+        if P1.head in shapes:
             kill(P1)
-        elif right_head in shapes:
+        elif P2.head in shapes:
             kill(P2)
         elif ground in shapes:
-            if left_head in shapes or left_foot in shapes:
+            if P1.head in shapes or P1.foot in shapes:
                 kill(P1)
-            elif right_head in shapes or right_foot in shapes:
+            elif P2.head in shapes or P2.foot in shapes:
                 kill(P2)
 
         return True
     space.add_collision_handler(0, 0, onCollision, None, None, None) 
 
     reset()
-
-    space.add(pm.PinJoint(body, left_thigh, l_hip, (0,0)))
-    left_thigh_muscle = pm.DampedRotarySpring(body, left_thigh, -(math.pi/4), MUSCLE_STRENGTH, MUSCLE_STIFFNESS)
-    left_calf_muscle = pm.DampedRotarySpring(left_thigh, left_calf, (math.pi/2), MUSCLE_STRENGTH, MUSCLE_STIFFNESS)
-    space.add(pm.PinJoint(left_thigh, left_calf, (0,-LEG_LEN), (0,0)))
-    space.add(left_thigh_muscle, left_calf_muscle)
-
-    space.add(pm.PinJoint(body, right_thigh, r_hip, (0,0)))
-    space.add(pm.PinJoint(right_thigh, right_calf, (0,-LEG_LEN), (0,0)))
-    right_thigh_muscle = pm.DampedRotarySpring(body, right_thigh, (math.pi/4), MUSCLE_STRENGTH, MUSCLE_STIFFNESS)
-    right_calf_muscle = pm.DampedRotarySpring(right_thigh, right_calf, -(math.pi/2), MUSCLE_STRENGTH, MUSCLE_STIFFNESS)
-    space.add(right_thigh_muscle, right_calf_muscle)
-
-    P1 = Player(left_thigh_muscle, left_calf_muscle, -1.0)
-    P2 = Player(right_thigh_muscle, right_calf_muscle, 1.0)
 
     STRETCH_RANGE = math.pi/5
     ANGLE_RANGE = math.pi/5
@@ -185,7 +194,7 @@ def main():
 
         ### Clear screen
         screen.fill(THECOLORS["white"])
-        
+
         ### Draw stuff
         for line in lines:
             body = line.body
@@ -195,11 +204,8 @@ def main():
             pt2 = to_pygame(pv2)
             pygame.draw.lines(screen, THECOLORS["black"], False, [pt1,pt2], 4)
 
-        for head in (left_head, right_head):
-            body = head.body
-            cv = body.position + head.offset.rotated(body.angle)
-            c = to_pygame(cv)
-            pygame.draw.circle(screen, THECOLORS["black"], c, int(head.radius))
+        P1.draw(screen)
+        P2.draw(screen)
 
         for line in (ground, mat):
             offset = line.body.position
